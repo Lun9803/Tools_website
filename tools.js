e979e6e85a905423b3a01caba1845f4c0280176c
Bolun Li <boli7059@uni.sydney.edu.au>
2018-12-16 19:38:37 +0800

    More functionalities
    * now can add & delete your own website shotcut
    * you can return to default shotcut

--- tools.js
+++ tools.js
@@ -1,11 +1,77 @@
 $(document).ready(function(){
-	var del = false;
+
+	if(localStorage.getItem("block")){
+		$(document.body).html(localStorage.getItem("block"));
+	}
+	
+	var add = $(this).find(".add");
+	add.hide();
+	
+	var del = $(this).find(".del");
+	del.hide();
+	
+	var del_img = $(this).find(".del_img");
+	del_img.hide();
 	
 	$(".title").click(function(){
 		var block = $(this).parent().find(".block");
 		var add = block.find(".add");
 		add.fadeToggle(300);
+		var del = block.find(".del");
+		del.fadeToggle(300);
+		
+		var del_img = block.find(".del_img");
+		del_img.hide(300);
 	});
+	
+	$(".add").click(function(){
+		var name = prompt("enter the name of the website");
+		if(name.length==0){
+			alert("name not entered");
+			return;
+		}
+		
+		var url = prompt("enter the url of the website\ne.g. www.lun.com");
+		if(url.length==0){
+			alert("url not entered");
+			return;
+		}
+		
+		if(url.charAt(url.length-1)=='/'){
+			url=url.slice(0,url.length-1);
+		}
+		
+		if(url.slice(0,8)!='https://'){
+			url='https://'+url;
+		}
+		
+		var e="<a class='nav' href="+url+">"+name+"</a>"+'<img src="img/del.jpg" class="del_img"></img>';
+		$(this).before(e);
+		
+		localStorage.setItem("block", $(document.body).html());
+	});
+		
+	$(".del").click(function(){
+		var block = $(this).parent();
+		var del_img = block.find(".del_img");
+		del_img.fadeToggle(300);
+	});
+	
+	$(".del_img").click(function(){
+		var target=$(this).prev();
+		if(confirm("确定删除"+target.html()+"?")){
+			target.remove();
+			$(this).remove();
+			localStorage.setItem("block", $(document.body).html());
+		}
 
+	});
+	
+	$("#initialize").click(function(){
+		if(confirm("确认初始化？")){
+			localStorage.clear();
+			window.location.reload();
+		}
+	})
 		
 });
